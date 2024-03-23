0086d0e7e72e0ce7066aec3ffb01d466151ec04e
flashworth <70212729+flashworth@users.noreply.github.com>
2024-03-23 20:22:00 +0000

    created BusinessEntry component

---
+++ src/components.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function BusinessEntry() {
+  return (
+    <div>
+      <img />
+      <h2>Name</h2>
+      <p>Address</p>
+      <p>city</p>
+      <p>State</p>
+      <p>zip code</p>
+      <p>Category</p>
+      <p>Rating</p>
+      <p>Review count</p>
+    </div>
+  )
+}
+
+export default BusinessEntry;
