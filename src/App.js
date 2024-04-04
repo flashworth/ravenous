cb9eae359e5b032fbc909adca873305532b5e918
flashworth <70212729+flashworth@users.noreply.github.com>
2024-04-04 15:44:41 +0100

    added 3 filter buttons

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import BusinessList from "./components/BusinessList";
 const App = () => {
   return (
     <div className="mb-20">
-      <h1 className="bg-amber-700 py-5 text-center text-4xl font-semibold text-white">
-        Ravenous
+      <h1 className="bg-black py-5 text-center text-4xl font-semibold text-white">
+        RAVENOUS
       </h1>
       <SearchBar />
       <BusinessList />
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,22 +3,36 @@ import React from "react";
 const SearchBar = () => {
   return (
     <div className="mx-auto max-w-4xl flex-grow py-10">
-      <div className="flex flex-col px-2 py-4">
-        <p className="font-bold">Restaraunt</p>
+      <div className="px-2">
+        <p className="font-bold">FILTER</p>
+        <div className="grid grid-cols-3 gap-2">
+          <button className="mt-2 md:w-32 rounded-lg border-2 border-black p-2 hover:bg-black hover:text-white">
+            Filter 1
+          </button>
+          <button className=" mt-2 md:w-32 rounded-lg border-2 border-black p-2 hover:bg-black hover:text-white">
+            Filter 2
+          </button>
+          <button className=" mt-2 md:w-32 rounded-lg border-2 border-black p-2 hover:bg-black hover:text-white">
+            Filter 3
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col px-2">
+        <p className="font-bold mt-4">RESTARAUNT</p>
         <input
-          className="mt-1 rounded-lg border-2 px-2 py-1"
+          className="mt-1 rounded-lg border-2 px-2 py-1 border-black"
           type="text"
           id="search"
           name="Search"
         />
-        <p className="mt-4 font-bold">Type</p>
+        <p className="mt-4 font-bold">TYPE</p>
         <input
-          className="mt-1 rounded-lg border-2 px-2 py-1"
+          className="mt-1 rounded-lg border-2 px-2 py-1 border-black"
           type="text"
           id="location"
           name="Location"
         />
-        <button className="my-2 mt-4 w-32 rounded-lg bg-amber-800 p-2 text-white">
+        <button className="my-2 mt-6 md:w-32 rounded-lg bg-black p-2 text-white">
           Search
         </button>
       </div>
