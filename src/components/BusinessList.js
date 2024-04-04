141a54d021dfafb4083763d3bb233f2645b6b3d4
flashworth <70212729+flashworth@users.noreply.github.com>
2024-04-04 15:30:56 +0100

    refined styles +1

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -3,7 +3,7 @@ import Business from "./Business";
 
 const BusinessList = () => {
   return (
-    <div className="container mx-auto grid max-w-lg gap-4 px-2 md:max-w-4xl md:grid-cols-3">
+    <div className="container mx-auto grid max-w-lg gap-12 px-2 md:max-w-4xl md:grid-cols-3">
       <Business />
       <Business />
       <Business />
